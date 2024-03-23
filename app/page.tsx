87b94d20b6024d44d4eb534a9aa2e5b1477a649a
jordan-bite <jordan@bite-interactive.com>
2024-03-22 17:35:56 -0700

    basic main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,94 +1,38 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div className={styles.center}>
+        <div className={styles.flexCenter}>
+          <h1 className={styles.title}>Demo App For</h1>
+        </div>
+        <div className={styles.flexCenter}>
+          <svg
+            className={styles.logo}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="37.2 366 960 201.8"
+            height={100}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <path d="M105.6 516.8H37.2V421h13.6v82h54.9v13.8zm61-95.9H153v95.8h13.6v-95.8zm592.5 47.8c0 26.5-21.6 47.8-47.8 47.8-26.5 0-47.8-21.6-47.8-47.8 0-10.4 3.4-20.2 9.3-28.1l-45.3 9.5c.2 1.6.4 3.1.4 5 0 13.6-7.7 25.2-19.3 30.8l20.2 30.8h-16.3l-17.9-27.4H573v27.4h-13.6v-52.4L475 482.1v34.7h-13.6V485L65.7 567.7v-12.5l189.9-39.9-51.6-94.4h15.6l37.8 69.3 37.8-69.3h15.6l-51.4 93.8 85.7-17.9v-75.9h68.4v13.6h-54.9v25.6h41v13.6h-41v20.2l610-127.9v12.5l-233.3 49c14.3 8.4 23.8 23.8 23.8 41.2zm-145.3-13.6c0-.7-.2-1.4-.2-2.1l-40.8 8.6v14.1h20.6c11.2 0 20.4-9.2 20.4-20.6zm131.6 13.6c0-18.8-15.4-34.3-34.3-34.3-18.8 0-34.3 15.4-34.3 34.3s15.4 34.3 34.3 34.3c18.9-.1 34.3-15.3 34.3-34.3zm120.8 16.7l-67.8-64.6h-.7v95.8h13.6V452l67.5 64.6h.9v-95.8h-13.6v64.6zm48.9-64.5v13.6h34.3v82H963v-82h34.3v-13.6h-82.2zm-440.6 13.6h54.9v-13.6H461v39.3l13.6-2.9v-22.8z"></path>
+          </svg>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={`${styles.content} ${styles.flexCenter}`}>
+        <div className={styles.flexCol}>
+          <a
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              Docs <span>-&gt;</span>
+            </h2>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+        </div>
       </div>
     </main>
   );
